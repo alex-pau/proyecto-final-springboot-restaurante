70a886f1d50d6b8664f4010d2d58332b10278fa1
AlexP <ajasmip@gmail.com>
2026-02-21 20:27:04 +0100

    chore: initial commit with spring boot and project dependencies

---
+++ src/test/java/com/proyectofinal/restaurante/ProyectoFinalRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
